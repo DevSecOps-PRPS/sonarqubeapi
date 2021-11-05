9b29f1dc0c2df9a9886137513d90bb6f680c5d57
unknown <hergi2004@gmail.com>
2021-11-05 15:12:33 -0400

    added sonarqube file

---
+++ sonarqubeapi/src/main/java/com/mht/exposeapi/service/SonarApiServiceImpl.java
@@ -0,0 +1,68 @@
+package com.mht.exposeapi.service;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.mht.exposeapi.dto.Measure;
+import com.mht.exposeapi.dto.MeasureHistory;
+import com.mht.exposeapi.dto.MeasureHistoryResponse;
+import com.mht.exposeapi.dto.Range;
+
+// import io.jsonwebtoken.Jwts;  
+// import io.jsonwebtoken.SignatureAlgorithm;  
+  
+// byte[] key = getSignatureKey();  
+  
+// String jwt = Jwts.builder().setIssuer("http://trustyapp.com/")  
+//     .setSubject("users/1300819380")  
+//     .setExpiration(expirationDate)  
+//     .put("scope", "self api/buy")  
+//     .signWith(SignatureAlgorithm.HS256,key)  
+//     .compact();
+
+@Service()
+public class SonarApiServiceImpl implements SonarApiService{
+
+	@Override
+	public List<List<Object>> getDataPoint(Range range, String metrics) {
+		List<List<Object>> datapoints = new ArrayList<>();
+		RestTemplate restTemplate = new RestTemplate();
+		String historyMetrics = "http://10.101.0.204:9000/api/measures/search_history";
+		UriComponentsBuilder builder = UriComponentsBuilder
+			    .fromUriString(historyMetrics)
+			    // Add query parameter
+			    //.queryParam("component", "com.infy.sonar:sonar-spring-demo")
+				.queryParam("token", "c6822504a1813787e4a87f47b1238645fd6ec5be")
+				.queryParam("component", "prps")
+			    .queryParam("metrics", metrics);
+		MeasureHistoryResponse measureHistoryResponse = restTemplate.getForObject(builder.toUriString(), MeasureHistoryResponse.class);
+		System.out.println("sonar response - "+measureHistoryResponse);
+		for(Measure measure: measureHistoryResponse.getMeasures()) {
+			for(MeasureHistory measureHistory: measure.getHistory()) {
+				ZonedDateTime zonedDateTime = measureHistory.getDate();
+				System.out.println("zoned time - "+zonedDateTime);
+				Long epochSecond = zonedDateTime.toEpochSecond();
+				LocalDateTime historyTime = zonedDateTime.toLocalDateTime();
+				LocalDateTime rangeFrom = range.getFrom();
+				LocalDateTime rangeTo = range.getTo();
+				System.out.println("history - "+historyTime);
+				System.out.println("from - "+rangeFrom);
+				System.out.println("to - "+rangeTo);
+				System.out.println("epoch sec - "+epochSecond);
+				if(historyTime.isBefore(range.getFrom()) || historyTime.isAfter(range.getTo()))
+					continue;
+				List<Object> datapoint = Arrays.asList(measureHistory.getValue(),epochSecond*1000);
+				datapoints.add(datapoint);
+			}
+		}
+		return datapoints;
+	}
+
+}
